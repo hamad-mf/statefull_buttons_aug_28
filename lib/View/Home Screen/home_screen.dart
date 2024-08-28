dc9f16a85ad5ec4c7af8595ccc0ef12ae179d6d9
hamad-mf <hamadmf100@gmail.com>
2024-08-28 20:08:40 +0530

    buttons

---
+++ lib/View/Home Screen/home_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String? dropvalue;
+  bool? a = true;
+  bool onoroff = true;
+  String? Gender = "";
+  bool r2 = true;
+  bool r3 = true;
+
+  List<String> countries = [
+    'United States',
+    'Canada',
+    'Australia',
+    'India',
+    'Germany',
+    'France',
+    'Brazil',
+    'Japan',
+    'South Africa',
+    'Mexico'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Checkbox(
+              value: a,
+              onChanged: (value) {
+                a = value;
+                print(a);
+                setState(() {});
+              }),
+          Switch(
+              value: onoroff,
+              onChanged: (value) {
+                onoroff = value;
+                setState(() {});
+              }),
+          RadioListTile(
+              title: Text("male"),
+              value: "male",
+              groupValue: Gender,
+              onChanged: (value) {
+                Gender = value;
+                setState(() {});
+              }),
+          RadioListTile(
+              title: Text("female"),
+              value: "female",
+              groupValue: Gender,
+              onChanged: (value) {
+                Gender = value;
+                setState(() {});
+              }),
+          RadioListTile(
+              title: Text("others"),
+              value: "others",
+              groupValue: Gender,
+              onChanged: (value) {
+                Gender = value;
+                setState(() {});
+              }),
+          PopupMenuButton(
+              itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Text("profile"),
+                      onTap: () {},
+                    ),
+                    PopupMenuItem(
+                      child: Text("settings"),
+                      onTap: () {},
+                    )
+                  ]),
+          DropdownButton(
+            value: dropvalue,
+            items: List.generate(
+              countries.length,
+              (index) => DropdownMenuItem(
+                child: Text(countries[index]),
+                value: countries[index],
+              ),
+            ),
+            onChanged: (value) {
+              print(value);
+              dropvalue = value;
+              setState(() {});
+            },
+          )
+        ]),
+      ),
+    );
+  }
+}
